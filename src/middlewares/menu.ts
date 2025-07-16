d6e0b026116b9faa5159fe1ead84e0e94e3202cc
Sudo Space <abolfazleffati1381@gmail.com>
2025-07-17 00:08:19 +0330

    disable routing options for now

--- src/middlewares/menu.ts
+++ src/middlewares/menu.ts
@@ -7,8 +7,9 @@ const menu = new Composer<BotContext>();
 menu.on("message:text", async (ctx) => {
   const keyboard = new InlineKeyboard();
 
-  keyboard.text(ctx.t("ir-direct"), "ir-direct").row();
-  keyboard.text(ctx.t("ch-direct"), "ch-direct").row();
+  //TODO: Fix Routing issue
+  // keyboard.text(ctx.t("ir-direct"), "ir-direct").row();
+  // keyboard.text(ctx.t("ch-direct"), "ch-direct").row();
   keyboard.text(ctx.t("no-direct"), "no-direct").row();
 
   ctx.reply(ctx.t("menu-message"), { reply_markup: keyboard });
